b5fcd8788aa97f79145cff5f4298c4dcbcfcf4db
Nigma <artnigman@gmail.com>
2026-09-11 10:52:08 +0500

    Initial commit: NetHunter kernel builder for begonia

---
+++ anykernel-begonia.sh
@@ -0,0 +1,99 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=NetHunter Kernel for begonia by opencode
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=begonia
+device.name2=begoniain
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+isTimRom() {
+  build_prop="/system/build.prop"
+  version_prop="ro.crdroid.build.version"
+
+  if grep -q "$version_prop" "$build_prop" && grep -q "timjosten" "$build_prop"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+isDerp() {
+  build_prop="/system/build.prop"
+  version_prop="ro.derp.fingerprint"
+
+  if grep -q "$version_prop" "$build_prop" && grep -q "DerpFest" "$build_prop"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Initialize block
+BLOCK=/dev/block/bootdevice/by-name/boot;
+
+if isTimRom; then
+    BLOCK=boot;
+    IS_SLOT_DEVICE=auto;
+    RAMDISK_COMPRESSION=none;
+
+    . tools/ak3-core.sh;
+
+    ui_print " Custom ROM recognition: Tim's Signature ";
+    ui_print " Initiating vibration fix deployment "
+    ui_print " Executing NoVA flash....";
+
+    split_boot;
+    patch_cmdline initcall_blacklist initcall_blacklist=
+    flash_boot;
+
+elif isDerp; then
+
+    BLOCK=/dev/block/by-name/boot;
+    IS_SLOT_DEVICE=0;
+    RAMDISK_COMPRESSION=auto;
+    NO_BLOCK_DISPLAY=true;
+
+    . tools/ak3-core.sh;
+
+    mount -o rw,remount -t auto /vendor >/dev/null;
+    restore_file /vendor/etc/init/hw/init.target.rc;
+
+    rm -rf $RAMDISK/overlay;
+    rm -rf $RAMDISK/overlay.d;
+
+    ui_print " Custom ROM recognition: DerpFest's detected ";
+    ui_print " Executing NoVA flash....";
+
+    dump_boot;
+    write_boot;
+
+else
+
+    IS_SLOT_DEVICE=0;
+    RAMDISK_COMPRESSION=auto;
+    NO_BLOCK_DISPLAY=true;
+
+    . tools/ak3-core.sh;
+
+    mount -o rw,remount -t auto /vendor >/dev/null;
+    restore_file /vendor/etc/init/hw/init.target.rc;
+
+    rm -rf $RAMDISK/overlay;
+    rm -rf $RAMDISK/overlay.d;
+
+    ui_print " Executing NoVA flash....";
+
+    dump_boot;
+    write_boot;
+
+fi
